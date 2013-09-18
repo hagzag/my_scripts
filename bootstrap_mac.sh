aefedf64ea4211d679cfbd5b105da4499afd0ba5
Haggai Philip Zagury <hagzag@hagzag.com>
2013-09-18 18:32:22 +0300

    moving commons to common.sh ...

--- bootstrap_mac.sh
+++ bootstrap_mac.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-export info="\033[0;32m==> info ]\033[0m"
-export warning="\033[0;33m==> warn ]\033[0m"
-export error="\033[0;31m==> error ]\033[0m"
-
-check_root() {
-  if [[ $EUID -ne 0 ]]; then
-      echo -e "$error This script must be run as root\n$info You could also run: sudo $0" 1>&2
-      exit 1
-  fi
-}
+test -f ./common.sh || { curl -L -O https://raw.github.com/cookbase/my_scripts/master/common.sh; } && { source common.sh;}
 
 check_root
 echo -e "$info Looking for Git ..., if absent will install it ..."
   sudo bash < <(curl -L https://raw.github.com/hagzag/my_scripts/master/install_git.sh)
 echo -e "$info Looking for MAC OSX CLI TOOLS, if absent will install it ..."
   sudo bash < <(curl -L https://raw.github.com/hagzag/xcode-cli-install/master/install.sh)
+
--- install_git.sh
+++ install_git.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -e
-
-info="\033[0;32m==> info ]\033[0m"
-warning="\033[0;33m==> warn ]\033[0m"
-error="\033[0;31m==> error ]\033[0m"
-
-check_root() {
-  if [[ $EUID -ne 0 ]]; then
-      echo -e "$error This script must be run as root\n$info You could also run: sudo $0" 1>&2
-      exit 1
-  fi
-}
+test -f ./common.sh || { curl -L -O https://raw.github.com/cookbase/my_scripts/master/common.sh; } && { source common.sh;}
 
 check_for_git() {
   result=`which git`
