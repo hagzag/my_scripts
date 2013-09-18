c2de1ab092606c2cf4f90a5913d384224a26606f
Haggai Philip Zagury <hagzag@hagzag.com>
2013-09-18 17:35:01 +0300

    Beuitify echo's

--- bootstrap_mac.sh
+++ bootstrap_mac.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-info="\033[0;32m==> info ]\033[0m"
-warning="\033[0;33m==> warn ]\033[0m"
-error="\033[0;31m==> error ]\033[0m"
+export info="\033[0;32m==> info ]\033[0m"
+export warning="\033[0;33m==> warn ]\033[0m"
+export error="\033[0;31m==> error ]\033[0m"
 
 check_root() {
   if [[ $EUID -ne 0 ]]; then
@@ -13,7 +13,7 @@ check_root() {
 }
 
 check_root
-echo "$info Looking for Git ..., if absent will install it ..."
+echo -e "$info Looking for Git ..., if absent will install it ..."
   sudo bash < <(curl -L https://raw.github.com/hagzag/my_scripts/master/install_git.sh)
-echo "$info Looking for MAC OSX CLI TOOLS, if absent will install it ..."
+echo -e "$info Looking for MAC OSX CLI TOOLS, if absent will install it ..."
   sudo bash < <(curl -L https://raw.github.com/hagzag/xcode-cli-install/master/install.sh)
