9fb056392d2c24dbcc2d8c3b325d1960a0e46922
Haggai Philip Zagury <hagzag@hagzag.com>
2013-09-18 17:17:00 +0300

    mac bootstrap script + README update

---
+++ bootstrap_mac.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+info="\033[0;32m==> info ]\033[0m"
+warning="\033[0;33m==> warn ]\033[0m"
+error="\033[0;31m==> error ]\033[0m"
+
+check_root() {
+  if [[ $EUID -ne 0 ]]; then
+      echo -e "$error This script must be run as root\n$info You could also run: sudo $0" 1>&2
+      exit 1
+  fi
+}
+
+check_root
+echo "$info Looking for Git ..., if absent will install it ..."
+  sudo bash < <(curl -L https://raw.github.com/hagzag/my_scripts/master/install_git.sh)
+echo "$info Looking for MAC OSX CLI TOOLS, if absent will install it ..."
+  sudo bash < <(curl -L https://raw.github.com/hagzag/xcode-cli-install/master/install.sh)
