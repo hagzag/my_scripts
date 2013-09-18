2fe69ee88692f756f7e0b2a628ce846d083910c6
Haggai Philip Zagury <hagzag@hagzag.com>
2013-09-18 17:31:16 +0300

    Beuitify echo's

--- install_git.sh
+++ install_git.sh
@@ -37,7 +37,7 @@ install_dmg() {
   mountpath="$4"
   mac_vol=${5:-'/Volumes/Macintosh HD'}
   
-  echo -e "mounting $pkg_name"
+  echo -e "$info mounting $pkg_name"
   hdiutil mount -nobrowse ./$pkg_name
   installer -pkg "$mountpath/$mpkg" -target "$mac_vol"
   hdiutil unmount "$mountpath"
@@ -50,7 +50,7 @@ main () {
 
   if [[ `check_for_git` -ne 0 ]]; then
     set_git_vars
-    echo "installing git, version: $git_ver"
+    echo "$info installing git, version: $git_ver"
     pushd /tmp
     curl -L -O $git_dmg_url 
     # install the damn thing ...
@@ -59,12 +59,12 @@ main () {
     export PATH=$PATH:/usr/local/git/bin/
     popd
   else
-    echo git already installed
+    echo "$info git already installed"
   fi
 
   # Check local conifg - annoying to see commit on git hub with user@localhost ...
   (git config --get user.email &>/dev/null || git config --get user.name &>/dev/null ) || \
-  (echo -e "Git config seems empty I would do the following:" 
+  (echo -e "$warning Git config seems empty I would do the following:" 
    echo -e "\t git config --global user.name \"Your username\"" 
    echo -e "\t git config --global user.email youremail@exmaple.com \n")
 }
